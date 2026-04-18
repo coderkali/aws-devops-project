b2f28d7fab12c0ab18aee3e3ff9526ffb7520cf6
kaliprasad <coderkali@gmail.com>
2026-04-18 06:39:06 -0500

    infra: VPC with public and private subnets, NAT Gateway

--- infrastructure/vpc/main.tf
+++ infrastructure/vpc/main.tf
@@ -23,7 +23,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public" {
     count = length(var.public_subnet_cidrs)
     vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnet_cidrs.id
+    cidr_block = var.public_subnet_cidrs[count.index]
     availability_zone = data.aws_availability_zones.available.names[count.index]
     map_public_ip_on_launch = true
 
@@ -92,7 +92,7 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "public" {
    count          = length(var.public_subnet_cidrs)
-   subnet_id = aws_subnet.public[count.index]
+   subnet_id = aws_subnet.public[count.index].id
    route_table_id =  aws_route_table.public.id
 }
 
