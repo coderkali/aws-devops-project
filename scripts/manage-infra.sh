c98e2689ba831afb67c9200410901e7cce8bfee4
kaliprasad <coderkali@gmail.com>
2026-04-21 06:09:03 -0500

    helm: add charts for all 5 retail store microservices

--- scripts/manage-infra.sh
+++ scripts/manage-infra.sh
@@ -15,30 +15,37 @@ format_time() {
 up() {
   TOTAL_START=$SECONDS
 
-  echo "==> [1/5] Applying VPC"
+  echo "==> [1/6] Applying VPC"
   STEP_START=$SECONDS
   cd $INFRA_DIR/vpc && terraform apply -auto-approve
   echo "    VPC done in $(format_time $((SECONDS - STEP_START)))"
 
-  echo "==> [2/5] Applying EKS"
+  echo "==> [2/6] Applying EKS"
   STEP_START=$SECONDS
   cd $INFRA_DIR/eks && terraform apply -auto-approve
   echo "    EKS done in $(format_time $((SECONDS - STEP_START)))"
 
-  echo "==> [3/5] Configuring kubectl"
+  echo "==> [3/6] Configuring kubectl"
   STEP_START=$SECONDS
   aws eks update-kubeconfig --region us-east-1 --name devops-eks-cluster
   echo "    kubectl configured in $(format_time $((SECONDS - STEP_START)))"
 
-  echo "==> [4/5] Applying Addons"
+  echo "==> [4/6] Applying Addons"
   STEP_START=$SECONDS
   cd $INFRA_DIR/addons && terraform apply -auto-approve
   echo "    Addons done in $(format_time $((SECONDS - STEP_START)))"
 
-  echo "==> [5/5] Applying Kubernetes resources"
+  echo "==> [5/6] Applying Kubernetes resources"
   STEP_START=$SECONDS
   kubectl apply -f $K8S_DIR
   echo "    Kubernetes resources done in $(format_time $((SECONDS - STEP_START)))"
+
+  # ... existing steps ...
+
+  echo "==> [5/6] Applying Data Plane"
+  STEP_START=$SECONDS
+  cd $INFRA_DIR/data-plane && terraform init && terraform apply -auto-approve
+  echo "    Data plane done in $(format_time $((SECONDS - STEP_START)))"
 
   echo ""
   echo "================================"
@@ -54,17 +61,22 @@ down() {
   kubectl delete -f $K8S_DIR
   echo "    Kubernetes resources deleted in $(format_time $((SECONDS - STEP_START)))"
 
-  echo "==> [2/4] Destroying Addons"
+  echo "==> [2/5] Destroying Data Plane"
+  STEP_START=$SECONDS
+  cd $INFRA_DIR/data-plane && terraform destroy -auto-approve
+  echo "    Data plane destroyed in $(format_time $((SECONDS - STEP_START)))"
+
+  echo "==> [3/5] Destroying Addons"
   STEP_START=$SECONDS
   cd $INFRA_DIR/addons && terraform destroy -auto-approve
   echo "    Addons destroyed in $(format_time $((SECONDS - STEP_START)))"
 
-  echo "==> [3/4] Destroying EKS"
+  echo "==> [4/5] Destroying EKS"
   STEP_START=$SECONDS
   cd $INFRA_DIR/eks && terraform destroy -auto-approve
   echo "    EKS destroyed in $(format_time $((SECONDS - STEP_START)))"
 
-  echo "==> [4/4] Destroying VPC"
+  echo "==> [5/5] Destroying VPC"
   STEP_START=$SECONDS
   cd $INFRA_DIR/vpc && terraform destroy -auto-approve
   echo "    VPC destroyed in $(format_time $((SECONDS - STEP_START)))"
